cf85f0d4ce8259db0ae24a153ad17e2e7b4ce634
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2022-12-31 17:36:22 +1100

    Refactored content script - Issues fixed

--- extension/dist/background.js
+++ extension/dist/background.js
@@ -2,4 +2,5 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.create({ url: "popup/install.html" });
     chrome.storage.local.set({ applicationIsOn: true });
+    chrome.storage.local.set({ amountOfPlaysToSkip: 1 });
 });
