e27f9645e999d7480977aa5208de73a73083c2e8
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2023-01-04 14:05:01 +1100

    Set chrome storage on installation

--- extension/dist/background.js
+++ extension/dist/background.js
@@ -1,6 +1,14 @@
-// On install show html page and set applicationIsOn to true
+// On install show html page and set applicationIsOn to true, + settings
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.create({ url: "popup/install.html" });
     chrome.storage.local.set({ applicationIsOn: true });
+    chrome.storage.local.set({ filterByMaxLength: "none" });
+    chrome.storage.local.set({ filterByMinLength: "none" });
     chrome.storage.local.set({ amountOfPlaysToSkip: 1 });
+    chrome.storage.local.set({ scrollOnComments: true });
+    chrome.storage.local.set({ shortCutKeys: ["shift", "s"] });
+    chrome.storage.local.set({ shortCutInteractKeys: ["shift", "f"] });
+    chrome.storage.local.set({
+        filteredAuthors: ["Tyson3101"],
+    });
 });
