9f22f3abf1500c2e5106084910bd47ababde92ab
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2023-03-11 23:15:01 +1100

    Fix popup showing on other than install

--- extension/dist/background.js
+++ extension/dist/background.js
@@ -1,17 +1,48 @@
 // On install show html page and set applicationIsOn to true, + settings
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.create({ url: "popup/install.html" });
-    chrome.storage.local.set({ applicationIsOn: true });
-    chrome.storage.local.set({ filterByMaxLength: "none" });
-    chrome.storage.local.set({ filterByMinLength: "none" });
-    chrome.storage.local.set({ amountOfPlaysToSkip: 1 });
-    chrome.storage.local.set({ scrollOnComments: false });
-    chrome.storage.local.set({ shortCutKeys: ["shift", "s"] });
-    chrome.storage.local.set({ shortCutInteractKeys: ["shift", "f"] });
-    chrome.storage.local.set({
-        filteredAuthors: ["Tyson3101"],
-    });
+  // Check installion or updated
+  chrome.storage.local.get(
+    [
+      "applicationIsOn",
+      "filterByMaxLength",
+      "filterByMinLength",
+      "amountOfPlaysToSkip",
+      "scrollOnComments",
+      "shortCutKeys",
+      "shortCutInteractKeys",
+      "filteredAuthors",
+    ],
+    (result) => {
+      if (result.applicationIsOn == undefined) {
+        chrome.storage.local.set({ applicationIsOn: true });
+        chrome.tabs.create({ url: "popup/install.html" });
+      }
+      if (result.filterByMaxLength == undefined) {
+        chrome.storage.local.set({ filterByMaxLength: "none" });
+      }
+      if (result.filterByMinLength == undefined) {
+        chrome.storage.local.set({ filterByMinLength: "none" });
+      }
+      if (result.amountOfPlaysToSkip == undefined) {
+        chrome.storage.local.set({ amountOfPlaysToSkip: 1 });
+      }
+      if (result.scrollOnComments == undefined) {
+        chrome.storage.local.set({ scrollOnComments: false });
+      }
+      if (result.shortCutKeys == undefined) {
+        chrome.storage.local.set({ shortCutKeys: ["shift", "s"] });
+      }
+      if (result.shortCutInteractKeys == undefined) {
+        chrome.storage.local.set({ shortCutInteractKeys: ["shift", "f"] });
+      }
+      if (result.filteredAuthors == undefined) {
+        chrome.storage.local.set({
+          filteredAuthors: ["Tyson3101"],
+        });
+      }
+    }
+  );
 });
 chrome.runtime.onUpdateAvailable.addListener(() => {
-    chrome.runtime.reload();
+  chrome.runtime.reload();
 });
